9219cf306ad569c59e9e286c05f0b05086508ca7
Daniel Fernández <daniel.fernandez@11thlabs.org>
2022-01-31 00:28:16 +0100

    Adapted blacklisting to 3.0 expectations: specifically blacklist a series of classes in java.lang

--- thymeleaf-spring6/src/main/java/org/thymeleaf/spring6/expression/ThymeleafEvaluationContext.java
+++ thymeleaf-spring6/src/main/java/org/thymeleaf/spring6/expression/ThymeleafEvaluationContext.java
@@ -174,8 +174,8 @@ public final class ThymeleafEvaluationContext
                 throw new EvaluationException(
                         String.format(
                                 "Access is forbidden for type '%s' in Thymeleaf expressions. " +
-                                "Blacklisted packages are: %s. Whitelisted classes are: %s.",
-                                typeName, ExpressionUtils.getBlacklist(), ExpressionUtils.getWhitelist()));
+                                "Blacklisted classes are: %s.",
+                                typeName, ExpressionUtils.getBlacklist()));
             }
             return this.typeLocator.findType(typeName);
         }
@@ -203,8 +203,8 @@ public final class ThymeleafEvaluationContext
                     throw new EvaluationException(
                             String.format(
                                     "Calling methods is forbidden for type '%s' in Thymeleaf expressions. " +
-                                    "Blacklisted packages are: %s. Whitelisted classes are: %s.",
-                                    type.getName(), ExpressionUtils.getBlacklist(), ExpressionUtils.getWhitelist()));
+                                    "Blacklisted classes are: %s.",
+                                    type.getName(), ExpressionUtils.getBlacklist()));
                 }
             }
             return super.resolve(context, targetObject, name, argumentTypes);
